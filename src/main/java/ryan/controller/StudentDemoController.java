d31ad96dd4007c4329a87d328d5101e7a137e6ad
NhatQuangTa <nhatquangta2911@gmail.com>
2019-03-18 17:02:32 +0700

    + Add Edit Student Page (Show Content from Form)

--- src/main/java/ryan/controller/StudentDemoController.java
+++ src/main/java/ryan/controller/StudentDemoController.java
@@ -3,8 +3,11 @@ package ryan.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import ryan.form.StudentForm;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
 import java.util.Arrays;
+
 
 @Controller
 public class StudentDemoController {
@@ -12,13 +15,53 @@ public class StudentDemoController {
     @GetMapping(path = "/edit")
     public String editStudent(final Model model) {
 
-        StudentForm student = new StudentForm("Ryan Shaun", 10,
-                "Software Engineering", "Male",
-                Arrays.asList("EN", "VI"), Arrays.asList("Game", "Music", "Sport", "Code"));
-
+        StudentForm student = new StudentForm("Tạ Nhật Quang", 9, "An toàn thông tin", "Male",
+                Arrays.asList("Vietnamese", "English"), Arrays.asList("Sport", "Music"));
         model.addAttribute("student", student);
 
         return "editStudentDemo";
+    }
+
+    @PostMapping(path = "/edit")
+    public String editStudent(final Model model, @ModelAttribute("student") final StudentForm student) {
+        model.addAttribute("username", student.getName());
+        return "/welcome";
+    }
+
+    @ModelAttribute("majors")
+    public String[] getMajors() {
+        return new String[]{
+                "Công nghệ phần mềm",
+                "An toàn thông tin",
+                "Mạng",
+                "Hệ thống nhúng"
+        };
+    }
+
+    @ModelAttribute("genders")
+    public String[] getGenders() {
+        return new String[]{
+                "Male",
+                "Female"
+        };
+    }
+
+    @ModelAttribute("languages")
+    public String[] getLanguages() {
+        return new String[]{
+                "Vietnamese",
+                "English",
+                "Japanese"
+        };
+    }
+
+    @ModelAttribute("hobbies")
+    public String[] getHobbies() {
+        return new String[]{
+                "Sport",
+                "Music",
+                "Game"
+        };
     }
 
 }
