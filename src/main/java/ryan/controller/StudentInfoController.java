638c2346aed10a2fc2ee947d6b64d0fd76e3d1ad
NhatQuangTa <nhatquangta2911@gmail.com>
2019-03-22 13:10:22 +0700

    + Edit student with Spring Form

--- src/main/java/ryan/controller/StudentInfoController.java
+++ src/main/java/ryan/controller/StudentInfoController.java
@@ -3,7 +3,6 @@ package ryan.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 
 @Controller
 public class StudentInfoController {
